73c8368097dc19c5f8739cb88a199eb1a49bf433
asia-ko <zykova.doc@gmail.com>
2023-10-13 16:20:42 +0200

    mart

--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -1,9 +1,10 @@
 with weekday_avg as (
     select 
-        weekday, 
+        weekday,
+        year 
         avg(avgtemp_c) 
     from {{ref('prep_temp')}}
-    group by weekday
+    group by weekday, year
 )
 select * 
 from weekday_avg
