fe28507302367f77736239b794930ee0fd5068ac
asia-ko <zykova.doc@gmail.com>
2023-10-13 16:22:29 +0200

    prep

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -5,7 +5,7 @@ WITH temperature_daily AS (
 add_weekday AS (
     SELECT *,
         to_char(date, 'Day') AS weekday,
-        date_part('day', date) AS day_num
+        date_part('day', date) AS day_num,
         date_part('year', date) AS year
     FROM temperature_daily
 )
