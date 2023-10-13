9326989c66bfdef3920b2f4202b63289b64f0b22
asia-ko <zykova.doc@gmail.com>
2023-10-13 15:16:58 +0200

    mart

--- models/mart/weekly_temp.sql
+++ models/mart/weekly_temp.sql
@@ -6,7 +6,8 @@ with weekly_temp
             		min(avgtemp_c) as weekly_avg_min,
                     avg(maxtemp_c) as weekly_max,
                     avg(mintemp_c) as weekly_min,
-                    avg(totalprecip_mm) as avg_precip_mm
+                    avg(totalprecip_mm) as avg_precip_mm,
+                    avg(maxwind_kph) as avg_wind_kph
             		from {{ref('prep_temp')}}                  
                     group by (city, week)
                     order by week
