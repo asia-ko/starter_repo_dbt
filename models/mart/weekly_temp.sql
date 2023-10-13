7da300b5b4995931c27fa2ea9c56c5dcf64f292e
asia-ko <zykova.doc@gmail.com>
2023-10-13 14:34:18 +0200

    mart

--- models/mart/weekly_temp.sql
+++ models/mart/weekly_temp.sql
@@ -5,7 +5,7 @@ with weekly_temp
             		max(avgtemp_c) as weekly_avg_max,
             		min(avgtemp_c) as weekly_avg_min,
                     avg(maxtemp_c) as weekly_max,
-                    avg(mintemp_c) as weekly_max
+                    avg(mintemp_c) as weekly_min
             		from {{ref('prep_temp')}}                  
                     group by (city, week)
                     order by week
