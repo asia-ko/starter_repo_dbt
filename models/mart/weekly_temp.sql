9f9359ce6497b39e133ea6a8f25a16494e4c4548
asia-ko <zykova.doc@gmail.com>
2023-10-18 15:44:49 +0200

    mart

--- models/mart/weekly_temp.sql
+++ models/mart/weekly_temp.sql
@@ -1,5 +1,5 @@
 with weekly_temp 
- 			as (select city, lat, lon,
+ 			as (select city, lat, lon, country
             		date_part('week', date) as week,
                     date_part('year', date) as year,
             		avg(avgtemp_c) as weekly_avg,
@@ -10,7 +10,7 @@ with weekly_temp
                     avg(totalprecip_mm) as avg_precip_mm,
                     avg(maxwind_kph) as avg_wind_kph
             		from {{ref('staging_weather')}}                  
-                    group by (city, lat, lon, week, year)
-                    order by year, week, city, lat,lon
+                    group by (country, city, lat, lon, week, year)
+                    order by year, week, city, lat,lon, country
 )
 select * from weekly_temp
